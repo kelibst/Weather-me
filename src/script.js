9cc598093e98592c0eea56b97c1e8b07a1427ab3
kelibst <kboster17@gmail.com>
2020-08-13 11:14:09 +0000

    added datefns

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
 import {form} from './dom';
+import { format } from 'date-fns'
 const cityKey= "QqFCfLuVXyO7oxFlgu4nWgRjh5hYjgVA";
 const cityBase = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+const weatherKey = "67c0476c31125a1dbcfdd521a5a88903";
+
+
+
 `<div class="alert alert-warning alert-dismissible fade show" role="alert">
 <strong>Holy guacamole!</strong> You should check in on some of those fields below.
 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -14,14 +19,13 @@ function Templates(){
     return {
         ulTemplate(data){
            
-            `   <li><span>${data}</span><span>${data.Country.EnglishName}</span></li>`
+            `<li><span>${data}</span><span>${data.Country.EnglishName}</span></li>`
         }
     }
 }
 
 function weatherCheck(){
     return {
-        locationKey: `QqFCfLuVXyO7oxFlgu4nWgRjh5hYjgVA`,
 
         city: "",
 
@@ -34,14 +38,51 @@ function weatherCheck(){
 
             return data
         },
-        
-        
-        render(node, clas, data){
+        async getWeather(city, key){
+            const WeatherBase = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+            const response = await fetch(WeatherBase);
+            const data = await response.json();
+
+            return data
+        },
+        //setup templates for render function
+        templates(){
+            return {
+                searchUl(data){
+                    return ` <li><span>${data.EnglishName}</span><span>${data.Country.EnglishName}</span></li>`
+                } ,
+                detailsUl(data){
+                    return `<li><span>${data.name}</span><span>${data.sys.country}</span></li>
+                            <li><span>${data.weather[0].main}</span><span>${data.weather[0].description}</span></li>
+                            <li><span>Humidity</span><span>${data.main.humidity}</span></li>
+                            <li><span>Temperature</span><span> ${Math.ceil(data.main.temp - 273.15)}&deg;</span></li>`
+                }, 
+                mainWeather(data){
+                    let dating = format(new Date(), 'MM/dd/yyyy')
+                    return `
+                                <h2 class="deg font-weight-bolder display-1">${Math.ceil(data.main.temp - 273.15)}&deg;</h2>
+                                
+                                <div class="h6 location-time mx-3">
+                                    <h5 class="location-text display-4">${data.name} / ${data.sys.country}</h5>
+                                   ${dating}
+                                   
+                                </div>
+                                <div class="location-icons">
+                                    <i class="fas fa-cloud"></i>
+                                    <p class="text-capitalize">${data.weather[0].description}</p>
+                                </div>
+                            `
+                } 
+                           
+            }  
+        },
+        //function to render things on the screen
+        render(node, clas, template){
             const container = document.createElement('ul');
-            let searchUl= ` <li><span>${data.EnglishName}</span><span>${data.Country.EnglishName}</span></li>`;
+            node.innerHTML = "";
             container.classList = clas;
             container.innerHTML = "";
-            container.innerHTML = searchUl;
+            container.innerHTML = template;
             node.appendChild(container);
         },
 
@@ -55,18 +96,20 @@ function weatherCheck(){
                     })
                         data.forEach((val) => {
                                 
-                            this.render(document.querySelector('.search'), 'search-res', val);
+                            this.render(document.querySelector('.search'), 'search-res', this.templates().searchUl(val));
                             
                         })
                         
                 }else if(data.length < 5){
                     data.forEach((val) => {
-                        this.render(document.querySelector('.search'), 'search-res', val);
+                        this.render(document.querySelector('.search'), 'search-res', this.templates().searchUl(val));
+                        
                     })
                 }
                 
             }  
         },
+        
 
         startCheck(){
             form.addEventListener('submit', (e)=>{
@@ -76,6 +119,12 @@ function weatherCheck(){
                 .then(data => {
                     this.outputCity(data);
                     this.city = data[0].EnglishName;
+                    this.getWeather(this.city, weatherKey)
+                    .then(data => {
+                   
+                        this.render(document.querySelector('.details'), 'details-res', this.templates().detailsUl(data));
+                        this.render(document.querySelector('.bottom'), 'location d-flex text-white', this.templates().mainWeather(data));
+                    })
                 })
                 .catch(err => console.log(err));
             })
