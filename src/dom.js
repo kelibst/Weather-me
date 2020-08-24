f286f16ee196032c97a7fb1fb20e682d7a9a240c
kelibst <kboster17@gmail.com>
2020-08-24 07:52:45 +0000

    fixed linters

--- src/dom.js
+++ src/dom.js
@@ -6,23 +6,22 @@ const closeContainer = document.querySelector('.close-btn');
 const sideCol = document.querySelector('.side-col');
 const bgCover = document.querySelector('.bg-cover');
 
-const renderContents = () => {
-  return {
-    // setup templates for render function
-    templates() {
-      return {
-        searchUl(data, ind) {
-          return ` <li id="${ind}" class="searchLi"><span>${data.EnglishName}</span><span>${data.Country.EnglishName}</span></li>`;
-        },
-        detailsUl(data) {
-          return `<li><span>${data.name}</span><span>${data.sys.country}</span></li>
+const renderContents = () => ({
+  // setup templates for render function
+  templates() {
+    return {
+      searchUl(data, ind) {
+        return ` <li id="${ind}" class="searchLi"><span>${data.EnglishName}</span><span>${data.Country.EnglishName}</span></li>`;
+      },
+      detailsUl(data) {
+        return `<li><span>${data.name}</span><span>${data.sys.country}</span></li>
                               <li><span>${data.weather[0].main}</span><span>${data.weather[0].description}</span></li>
                               <li><span>Humidity</span><span>${data.main.humidity}</span></li>
                               <li><span>Temperature</span><span> ${Math.ceil(data.main.temp - 273.15)}&deg;</span></li>`;
-        },
-        mainWeather(data) {
-          const dating = format(new Date(), 'MM/dd/yyyy');
-          return `
+      },
+      mainWeather(data) {
+        const dating = format(new Date(), 'MM/dd/yyyy');
+        return `
                                   <h2 class="deg font-weight-bolder display-1">${Math.ceil(data.main.temp - 273.15)}&deg;</h2>
                                   
                                   <div class="h6 location-time mx-3">
@@ -35,19 +34,18 @@ const renderContents = () => {
                                       <p class="text-capitalize">${data.weather[0].description}</p>
                                   </div>
                               `;
-        },
-      };
-    },
-    // function to render things on the screen
-    render(node, clas, template) {
-      const container = document.createElement('ul');
+      },
+    };
+  },
+  // function to render things on the screen
+  render(node, clas, template) {
+    const container = document.createElement('ul');
 
-      container.classList = clas;
-      container.innerHTML = template;
-      node.appendChild(container);
-    },
-  };
-}
+    container.classList = clas;
+    container.innerHTML = template;
+    node.appendChild(container);
+  },
+});
 
 export {
   form, alertContainer, closeContainer, renderContents, sideCol, bgCover,
